460d89de70840a551873988a4d1bf54edf416da0
Harriet Chepngeno <harrietseroney2023@gmail.com>
2025-06-26 00:07:50 +0300

    Create a one-to-many relationship

--- lib/owner_pet.py
+++ lib/owner_pet.py
@@ -1,5 +1,42 @@
 class Pet:
-    pass
+    all = []
+    PET_TYPES = ["dog", "cat", "rodent", "bird", "reptile", "exotic"]
+    def __init__(self,name,pet_type,owner=None):
+        self.name = name
+        self.pet_type = pet_type
+        self.owner = owner
+        Pet.all.append(self)
+        if pet_type not in Pet.PET_TYPES:
+            raise Exception (f"{pet_type} is not a valid pet")
 
 class Owner:
-    pass
+    def __init__(self,name):
+        self.name = name
+        
+
+    def pets(self):
+        return [pet for pet in Pet.all if pet.owner == self]
+        
+    
+    def add_pet(self,pet):
+        if isinstance(pet,Pet):
+         pet.owner =self
+        else:
+            raise Exception("Only Pet instances can be added.")
+        
+    def get_sorted_pets(self):
+        sorted_list = sorted(self.pets(), key=lambda pet: pet.name)
+        return sorted_list
+
+owner = Owner("Harriet")
+
+dog = Pet("Simba", "dog")
+cat = Pet("Milo", "cat")
+bird = Pet("Zazu", "bird")
+
+owner.add_pet(dog)
+owner.add_pet(cat)
+owner.add_pet(bird)
+
+for pet in owner.get_sorted_pets():
+    print(pet.name)
